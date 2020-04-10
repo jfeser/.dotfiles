19631c990bbb8cb4db888161857ea50069ab035d
Jack Feser <feser@csail.mit.edu>
2020-04-10 11:54:45 -0400

    Add dotfiles.

---
+++ fish/.config/fish/functions/mosh-sketch2.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function mosh-sketch2 --description 'alias mosh-sketch2 mosh --ssh="ssh -K" feser@sketch2.csail.mit.edu'
+	mosh --ssh="ssh -K" feser@sketch2.csail.mit.edu $argv;
+end
