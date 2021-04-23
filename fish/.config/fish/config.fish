51b5bcd24415e8de8710e169bc6923fa120c2a83
Jack Feser <feser@csail.mit.edu>
2021-04-23 18:22:43 -0400

    fish: update path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,4 +6,7 @@ opam config env --shell=fish | source
 # Direnv
 eval (direnv hook fish)
 
-
+set -x PATH $HOME/.cargo/bin:$PATH
+set -x PATH $HOME/bin:$PATH
+set -x VISUAL emacs
+set -x EDITOR emacs
